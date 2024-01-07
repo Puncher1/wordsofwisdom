efc2f638cadb7bdc54964dc00d45e8e29c080d88
Puncher <65789180+Puncher1@users.noreply.github.com>
2024-01-07 20:37:19 +0100

    Move image.py from utils

--- bot/tweet.py
+++ bot/tweet.py
@@ -2,9 +2,10 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import mail_support
 from .api.client import Client
 from .utils.common import MAX_POST_LENGTH
-from .utils.image import create_quote_image
+from .image import create_quote_image
 
 if TYPE_CHECKING:
     from api.quotes import Quote
