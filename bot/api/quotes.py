0f289effb6a4424027a0d25c1fb2ed48c4c9d9fe
Puncher <65789180+Puncher1@users.noreply.github.com>
2024-01-08 08:55:29 +0100

    Add user agent header + status code error message

--- bot/api/quotes.py
+++ bot/api/quotes.py
@@ -1,6 +1,9 @@
 import random
+import sys
+
 import requests
 from typing import TypedDict, List
+from http.client import responses
 
 from ..utils.common import CATEGORIES
 
@@ -32,14 +35,23 @@ class Client:
         self.token = token
 
     def _fetch_quote(self, category: str) -> List[QuotePayload]:
-        headers = {"X-Api-Key": self.token}
+        headers = {
+            "X-Api-Key": self.token,
+            "User-Agent": "https://github.com/puncher1/wordsofwisdom twitter-bot Python/{0[0]}.{0[1]} requests/{1}".format(
+                sys.version_info, requests.__version__
+            ),
+        }
         response = requests.get(self.API_URL.format(category), headers=headers)
 
         data = response.json()
         if response.status_code == requests.codes.ok:
             return data
         else:
-            error_msg = data["error"]
+            try:
+                error_msg = data["error"]
+            except KeyError:
+                error_msg = responses[response.status_code]
+
             raise HTTPException(f"Request failed (code {response.status_code}): {error_msg}")
 
     def get_random_quote(self) -> Quote:
